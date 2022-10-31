5f43fc3a64a62036263dac6e513fec6d1a7f2c16
= <=>
2022-10-31 18:21:40 +0530

    refactored

--- src/components/productList.js
+++ src/components/productList.js
@@ -1,9 +1,22 @@
 import React from 'react'
 
-function productList() {
+function ProductList() {
   return (
-    <div>productList</div>
+    <div>
+      <h1 className='text-center text-info'>Product List</h1>
+      <div className='container-fluid mx-2'></div>
+      <div className='row mt-3 mx-2'>
+        <div className='col-md-4'>
+          <button className='btn btn-warning'>computers</button>
+          <button className='btn btn-warning'>Laptops</button>
+          <button className='btn btn-warning'>Servers</button>
+        </div>
+        {/* <div className='col-md-9'>
+        </div> */}
+      </div>
+    </div>
+
   )
 }
 
-export default productList
+export default ProductList
