a069fcf2a1ef77def0abe9a2f67a388b4f64ffb0
Sushant R Kundarnad <sushantrrk@gmail.com>
2022-11-08 22:02:46 +0530

    changes

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,10 +1,23 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 
 function Cart(props) {
+  const [qty,setQty]=useState([0,0,0,0,0,0,0,0,0]);
+  var cart=new Map()
+  var temp=[]
+
+  useEffect=()=>{
+    temp.push({
+      name:props.name,
+      price:props.price
+    })
+    cart.push()
+  }
+
   return (
     <div>
         <ul>
-            <li key={props.pid}>{props.name}    {props.price}</li>
+            <li key={props.pid}>{props.name}  {qty[props.pid-100]}  {props.price}</li>
+            
         </ul>
     </div>
   )
--- src/components/Home.js
+++ src/components/Home.js
@@ -2,10 +2,12 @@ import React from 'react'
 
 function Home(props) {
   return (
-    <div style={{display:"flex", justifyContent:"space-between"}}>
-        {props.cartCount}
-      <h1 className='text-center'>Shopping Cart</h1>
-      <img src="https://image.shutterstock.com/image-vector/shopping-cart-vector-icon-flat-260nw-1690453492.jpg" alt='Cart' width="60px" height="60px "></img>
+    <div style={{display:"flex","justifyContent":"center"}}>
+        <h1>Shopping Cart</h1>
+      <div style={{"justifyContent":"end","backgroundColor":"red"}}>  
+        <img src="https://image.shutterstock.com/image-vector/shopping-cart-vector-icon-flat-260nw-1690453492.jpg" alt='Cart' width="60px" height="60px "></img>
+        <label>{props.cartCount}</label> 
+      </div>
 
     </div>
   )
