9421c83ba2f0cda7dcc56f6385c69807120a2df5
SBTopZZZ-LG <sbtopinkatti@gmail.com>
2023-02-08 21:25:23 +0530

    Fetch data from back-end

--- src/components/productList.js
+++ src/components/productList.js
@@ -2,23 +2,18 @@ import React, {useState} from 'react'
 import Home from "./Home";
 import Cart from "./Cart";
 import "./Cart.css"
-let prod = {
-  Computers: 
-    [{pid:1,pname:"Mac mini",price:80000, ImageUrl:"https://d2d22nphq0yz8t.cloudfront.net/88e6cc4b-eaa1-4053-af65-563d88ba8b26/https://media.croma.com/image/upload/v1606585990/Croma%20Assets/Computers%20Peripherals/Desktops/Images/9009476698142.png/mxw_2256,s_jp2,s_videoimg,ns_atwebp,f_auto"},
-    {pid:2,pname:"Ryzen 5",price:60000, ImageUrl:"https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcQ_pT7b9V4Gtws5ZOjwjk3i0yXziiVdnPiTq211JEKYErTJ4r-bpl1js3VOMG-B0DIyqYXQ4yq3c-U&usqp=CAc"},
-    {pid:3,pname:"Intel i9",price:50000, ImageUrl:"https://www.thevaluestore.in/image/cache/catalog/2021/Intel/core-i9-12900k-1-1000x1000.jpg"}],
-  
-  Laptops: 
-    [{pid:4,pname:"Macbook Air",price:80000, ImageUrl:"https://d2d22nphq0yz8t.cloudfront.net/88e6cc4b-eaa1-4053-af65-563d88ba8b26/https://media.croma.com/image/upload/v1666277779/Croma%20Assets/Computers%20Peripherals/Tablets%20and%20iPads/Images/256712_0_y4ghxi.png/mxw_2256,s_jp2,s_videoimg,ns_atwebp,f_auto"},
-    {pid:5,pname:"ASUS TUF",price:68000, ImageUrl:"https://d2d22nphq0yz8t.cloudfront.net/88e6cc4b-eaa1-4053-af65-563d88ba8b26/https://media.croma.com/image/upload/v1664417481/Croma%20Assets/Computers%20Peripherals/Laptop/Images/256263_0_hhjvoi.png/mxw_2256,s_jp2,s_videoimg,ns_atwebp,f_auto"},
-    {pid:6,pname:"Surface GO",price:70900, ImageUrl:"https://d2d22nphq0yz8t.cloudfront.net/88e6cc4b-eaa1-4053-af65-563d88ba8b26/https://media.croma.com/image/upload/v1664415757/Croma%20Assets/Computers%20Peripherals/Laptop/Images/257307_0_w9hown.png/mxw_2256,s_jp2,s_videoimg,ns_atwebp,f_auto"}],
-  
-  Servers: 
-    [{pid:7,pname:"Dell Inspiron Desktop",price:70989, ImageUrl:"https://i.dell.com/is/image/DellContent/content/dam/ss2/product-images/dell-client-products/desktops/inspiron-desktops/inspiron-3910-desktop/media-gallery/in3910-csy-00000ff-bl-noodd-nomcr.psd?fmt=png-alpha&pscan=auto&scl=1&hei=402&wid=362&qlt=100,1&resMode=sharp2&size=362,402&chrss=full"},
-    {pid:8,pname:"Dell OptiPlex 7000",price:93267, ImageUrl: "https://i.dell.com/is/image/DellContent/content/dam/ss2/products/desktops-and-all-in-ones/optiplex/7000-tsff/media-gallery/optiplex-7000t-gallery-1.psd?fmt=png-alpha&pscan=auto&scl=1&hei=402&wid=402&qlt=100,1&resMode=sharp2&size=402,402&chrss=full"},
-    {pid:9,pname:"Dell T440 tower server",price:132000, ImageUrl: "https://www.eagle.in/wp-content/uploads/2018/10/Dell-T440-Server.jpg"}]
-}
-var qty=[0,0,0,0,0,0,0,0,0];
+
+let prod = new Promise((resolve, reject) => {
+  fetch("https://shopping-cart-backend.sbtopzzzlg.repl.co/products")
+    .then(response => {
+      response.json()
+        .then(json => resolve(json.data.products))
+        .catch(reject);
+    })
+    .catch(reject);
+});
+
+var qty = {};
 var cart=[]
 
 function ProductList() {
@@ -26,18 +21,18 @@ function ProductList() {
   const [AllProds,setProds] = useState([]);
   const [cart,setCart] = useState([]);
 
-  const handleClickComputers=()=>{
-    let Allcomputers= prod.Computers;
+  const handleClickComputers= async ()=>{
+    let Allcomputers = (await prod).filter(product => product.category === "Computers");
     setProds(Allcomputers);
   }
 
-  const handleClickLaptops=()=>{
-    let AllLaptops= prod.Laptops;
+  const handleClickLaptops= async ()=>{
+    let AllLaptops= (await prod).filter(product => product.category === "Laptops");
     setProds(AllLaptops);
   }
 
-  const handleClickServers=()=>{
-    let AllServers= prod.Servers;
+  const handleClickServers= async ()=>{
+    let AllServers= (await prod).filter(product => product.category === "Servers");
     setProds(AllServers);
   }
 
@@ -47,12 +42,15 @@ function ProductList() {
     setCount(count+1);
     let item = {};
     if(cart.length == 0){
-      item.pid = AllProds.find(item=>(item.pid===id)).pid;
-      item.name = AllProds.find(item=>(item.pid===id)).pname;
-      item.price = AllProds.find(item=>(item.pid===id)).price;
+      item.pid = AllProds.find(item=>(item._id===id))._id;
+      item.name = AllProds.find(item=>(item._id===id)).name;
+      item.price = AllProds.find(item=>(item._id===id)).price;
       console.log("1");
-      setCart(cart=>[...cart,item]);
-      qty[id-1]=qty[id-1]+1;
+      setCart(cart => [...cart, item]);
+      if (id in qty)
+        qty[id] = qty[id] + 1;
+      else
+        qty[id] = 1;
     }
     else{ 
       for (let i = 1; i < cart.length+1; i++) {
@@ -66,14 +64,20 @@ function ProductList() {
         }
       }
       if(flag==0){
-        item.pid = AllProds.find(item=>(item.pid===id)).pid;
-        item.name = AllProds.find(item=>(item.pid===id)).pname;
-        item.price = AllProds.find(item=>(item.pid===id)).price;
-        qty[id-1]=qty[id-1]+1;
+        item.pid = AllProds.find(item=>(item._id===id))._id;
+        item.name = AllProds.find(item=>(item._id===id)).name;
+        item.price = AllProds.find(item=>(item._id===id)).price;
+        if (id in qty)
+          qty[id] = qty[id] + 1;
+        else
+          qty[id] = 1;
         setCart(cart=>[...cart,item]);
       }
       else{
-        qty[id-1]=qty[id-1]+1;
+        if (id in qty)
+          qty[id] = qty[id] + 1;
+        else
+          qty[id] = 1;
 
       }
     }
@@ -94,11 +98,11 @@ function ProductList() {
 
               <ul className='bg-red' style = {{"display":"flex", "align-items":"center","list-style":"none"}}>
               {AllProds.map(prod=>
-                <li style={{"flex":"0 0 1", "margin":"10px"}} key={prod.pid}>
-                  {prod.pname} 
-                  <img src={prod.ImageUrl} alt="image" width="100px" height="100px" /> 
+                <li style={{"flex":"0 0 1", "margin":"10px"}} key={prod._id}>
+                  {prod.name} 
+                  <img src={prod.image} alt="image" width="100px" height="100px" /> 
                   {prod.price} 
-                  <button className='btn btn-secondary' onClick={()=>addtocart(prod.pid)}>Add To Cart</button> </li>)}
+                  <button className='btn btn-secondary' onClick={()=>addtocart(prod._id)}>Add To Cart</button> </li>)}
               </ul>
 
 
@@ -117,7 +121,7 @@ function ProductList() {
             </tr>
           </table>
           {cart.map(item=>(
-            <Cart pid={item.pid} name={item.name} price={item.price} qty={qty[item.pid-1]}/>
+            <Cart pid={item.pid} name={item.name} price={item.price} qty={qty[item.pid]}/>
           ))}
         </div>
       </div>
